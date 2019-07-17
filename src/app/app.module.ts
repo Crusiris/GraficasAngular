03d4082fe51a89cdec8e2b398fef877cdb8f4cc5
crusiris <cruzsirys@gmail.com>
2019-07-17 13:56:26 -0400

    grafica lineal y grafica de barras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Graficos
+import { ChartsModule } from 'ng2-charts';
+
+//componentes
 import { AppComponent } from './app.component';
+import { LineaComponent } from './components/linea/linea.component';
+import { BarComponent } from './components/bar/bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineaComponent,
+    BarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
